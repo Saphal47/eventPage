b8ffcd4941c787ac1bd347666aa0a9736863428c
Sal <81916015+Saphal47@users.noreply.github.com>
2024-01-13 23:52:14 +0530

    Update index.jsx

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -11,13 +11,26 @@ export const Card = () => {
   const [branch,setBranch] = useState('');
   const [year,setYear] = useState('');
   const [phone,setPhone] = useState('');
+  const [alertMessage,setAlertMessage] = useState('');
 
+  const onSubmit = async ()=>{
+    if (!name || !email || !college || !branch || !year || !phone) {
+      setAlertMessage('Registration failed. Please fill in all mandatory fields.');
+      return;
+    }
+    
+    try{ 
+      await axios.post('https://sheet.best/api/sheets/de011e04-d8e4-4d3f-b445-1a03812790e4',{
+        name,email,college,
+        branch,year,phone
+      });
+      setAlertMessage('Registeration Successful!');
+    }
+    catch(error){
+      console.log('Error try again:',error);
+      setAlertMessage('Registeration failed. Try again!');
+    }
 
-  const onSubmit = ()=>{
-    axios.post('https://sheet.best/api/sheets/de011e04-d8e4-4d3f-b445-1a03812790e4',{
-      name,email,college,
-      branch,year,phone
-    })
   }
   const openRegistrationForm = () => {
     setIsModalOpen(true);
@@ -25,6 +38,7 @@ export const Card = () => {
 
   const closeRegistrationForm = () => {
     setIsModalOpen(false);
+    setAlertMessage('');
   };
 
   
@@ -55,85 +69,90 @@ export const Card = () => {
         >
           <h2 className='text-2xl text-center font-bold'>Registration Form</h2>
           <form className="max-w-md mx-auto">
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-              Name:
-            </label>
-            <input
-              className="w-full border border-gray-300 p-2 rounded-md"
-              type="text"
-              placeholder='Your name'
-              onChange={(e)=> setName(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                Name:
+              </label>
+              <input
+                className="w-full border border-gray-300 p-2 rounded-md"
+                type="text"
+                placeholder='Your name'
+                onChange={(e)=> setName(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              Email:
-            </label>
-            <input
-              className="w-full border border-gray-300 p-2 rounded-md"
-              type="text"
-              placeholder='123@gmail.com'
-              onChange={(e)=> setEmail(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                Email:
+              </label>
+              <input
+                className="w-full border border-gray-300 p-2 rounded-md"
+                type="text"
+                placeholder='123@gmail.com'
+                onChange={(e)=> setEmail(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="college">
-              College:
-            </label>
-            <input
-              className="w-full border border-gray-300 p-2 rounded-md"
-              type="text"
-              placeholder='college name'
-              onChange={(e)=> setCollege(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="college">
+                College:
+              </label>
+              <input
+                className="w-full border border-gray-300 p-2 rounded-md"
+                type="text"
+                placeholder='college name'
+                onChange={(e)=> setCollege(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="branch">
-              Branch:
-            </label>
-            <input
-              className="w-full border border-gray-300 p-2 rounded-md"
-              type="text"
-              placeholder='Branch'
-              onChange={(e)=> setBranch(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="branch">
+                Branch:
+              </label>
+              <input
+                className="w-full border border-gray-300 p-2 rounded-md"
+                type="text"
+                placeholder='Branch'
+                onChange={(e)=> setBranch(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
-              Graduation Year:
-            </label>
-            <input
-              className="w-full border border-gray-300 p-2 rounded-md"
-              type='number'
-              placeholder='2020'
-              onChange={(e)=> setYear(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+                Graduation Year:
+              </label>
+              <input
+                className="w-full border border-gray-300 p-2 rounded-md"
+                type='number'
+                placeholder='2020'
+                onChange={(e)=> setYear(e.target.value)}
+              />
+            </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
-              Phone:
-            </label>
-            <input
-              className="w-full border border-gray-300 p-2 rounded-md"
-              type='number'
-              placeholder='8888888888'
-              onChange={(e)=> setPhone(e.target.value)}
-            />
-          </div>
+            <div className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="phone">
+                Phone:
+              </label>
+              <input
+                className="w-full border border-gray-300 p-2 rounded-md"
+                type='number'
+                placeholder='8888888888'
+                onChange={(e)=> setPhone(e.target.value)}
+              />
+            </div>
 
-          <button
-            type="button"
-            className="block w-full bg-primary px-6 py-2.5 text-xs font-medium uppercase rounded-md text-white bg-blue-700"
-            onClick = {onSubmit}
-          >
-            Register
-          </button>
+            <button
+              type="button"
+              className="block w-full bg-primary px-6 py-2.5 text-xs font-medium uppercase rounded-md text-white bg-blue-700"
+              onClick = {onSubmit}
+            >
+              Register
+            </button>
+              {alertMessage && (
+                <div className="mt-4 p-2 bg-green-200 text-green-800 rounded-md">
+                  {alertMessage}
+                </div>
+              )}
         </form>
 
         </Modal>
